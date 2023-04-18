1665b6bd0f4abe996744dc3c132afee7c68b2c51
Samed Kızılhan <samedkizilhan@Samed-MacBook-Air.local>
2023-04-18 21:39:05 +0300

    Initial commit

---
+++ src/samed_kizilhan.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class samed_kizilhan {
+    public static void main(String[] args) {
+
+        StdDraw.enableDoubleBuffering(); // for better animation
+
+        Environment.setCanvasSize();
+
+        Environment.displayBackground();
+
+        Player.playerInAction();
+
+
+
+    }
+}
